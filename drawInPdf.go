fcdca866e17cc559e9a1641f0079cd42716046c1
Don4ikkryt <don4ikkryt@gmail.com>
2019-10-26 20:55:49 +0300

    Add background

--- drawInPdf.go
+++ drawInPdf.go
@@ -9,10 +9,13 @@ import (
 func main() {
 	f := gofpdf.New("P", "mm", "A4", "")
 	f.AddPage()
+
+	f.SetFillColor(0, 153, 255)
+	f.Rect(10, 10, 100, 200, "F")
 	f.SetFillColor(255, 0, 0)
 	f.SetLineWidth(0.1)
 	f.Circle(30, 40, 2, "F")
-	file := "C:\\Users\\don4i\\Desktop\\Go\\PDF\\dre"
+	file := "C:\\Users\\don4i\\Desktop\\Go\\PDF\\drp"
 	err := f.OutputFileAndClose(file)
 	if err != nil {
 		fmt.Println("err")
